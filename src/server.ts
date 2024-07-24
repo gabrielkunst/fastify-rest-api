7b9b39fe1a074988910e790ccef187c41c8f6d35
Gabriel Kunst <gabrielkt.dev@gmail.com>
2024-07-23 23:37:56 -0300

    chore: add testing configs and packages

--- src/server.ts
+++ src/server.ts
@@ -1,41 +1,5 @@
-import fastify from "fastify";
 import { env } from "./env";
-import { transactionsRoutes } from "./routes/transactions";
-import {
-  jsonSchemaTransform,
-  serializerCompiler,
-  validatorCompiler,
-  ZodTypeProvider,
-} from "fastify-type-provider-zod";
-import fastifyCookie from "@fastify/cookie";
-import fastifySwaggerUi from "@fastify/swagger-ui";
-import fastifySwagger from "@fastify/swagger";
-
-const app = fastify().withTypeProvider<ZodTypeProvider>();
-
-app.register(fastifySwagger, {
-  openapi: {
-    info: {
-      title: "Transactions REST API",
-      description:
-        "Transactions REST API built with Fastify, tested using Vitest and Supertest, and documented with Swagger",
-      version: "1.0.0",
-    },
-  },
-  transform: jsonSchemaTransform,
-});
-
-app.register(fastifySwaggerUi, {
-  routePrefix: "/docs",
-});
-
-app.setSerializerCompiler(serializerCompiler);
-app.setValidatorCompiler(validatorCompiler);
-
-app.register(fastifyCookie);
-app.register(transactionsRoutes, {
-  prefix: "/transactions",
-});
+import { app } from "./app";
 
 app
   .listen({
